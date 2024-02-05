e9c8dbdcd90ea9cdc79424c71f51a9208d20a71d
Clement-Z4RM <clement.lienard@epitech.eu>
2024-02-05 19:44:10 +0100

    test: add test for no argument

---
+++ tests/test_no_argument.cpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** test_no_argument.cpp
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include <iostream>
+#include "NanoTekSpice.hpp"
+
+static const char *ARGV[] = {"./nanotekspice", nullptr};
+
+static void redirectAllStd() {
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
+
+Test(nts, no_argument, .init = redirectAllStd)
+{
+    cr_assert_eq(nts::NanoTekSpice::run(ARGV), 84);
+
+    std::cout.flush();
+    std::cerr.flush();
+
+    cr_assert_stdout_eq_str("");
+    cr_assert_stderr_eq_str("./nanotekspice: No file provided\n");
+}
