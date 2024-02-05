ffaee30bd85b5764877749ab1b35014e24441098
Clement-Z4RM <clement.lienard@epitech.eu>
2024-02-05 19:43:52 +0100

    feat: add error message if no file is provided

--- src/NanoTekSpice.cpp
+++ src/NanoTekSpice.cpp
@@ -5,8 +5,13 @@
 ** NanoTekSpice.cpp
 */
 
+#include <iostream>
 #include "NanoTekSpice.hpp"
 
 int nts::NanoTekSpice::run([[maybe_unused]] const char *argv[]) {
+    if (!argv[1]) {
+        std::cerr << argv[0] << ": No file provided" << std::endl;
+        return 84;
+    }
     return 0;
 }
