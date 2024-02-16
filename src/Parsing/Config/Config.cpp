09f5483c262f8a1e86b0944326574a0b4d590d87
Clement-Z4RM <clement.lienard@epitech.eu>
2024-02-16 19:08:17 +0100

    feat: add `Config` class
    It's a class that opens file from program arguments with some checks (no argument, `-h`/`--help`...)

---
+++ src/Parsing/Config/Config.cpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** Config.cpp
+*/
+
+#include <filesystem>
+#include <iostream>
+#include <cstring>
+#include "../../../include/NanoTekSpice.hpp"
+#include "Config.hpp"
+
+nts::Config::Config(const char *filename) : _isOpen(false), _filename(filename ? filename : "") {
+    if (_filename.empty()) {
+        std::cerr << program_invocation_name << ": No file provided" << std::endl;
+        return;
+    }
+    if (_filename == "-h" || _filename == "--help")
+        nts::NanoTekSpice::help();
+    if (!std::filesystem::is_regular_file(_filename)) {
+        std::cerr << program_invocation_name << ": " << _filename << ": Not a regular file" << std::endl;
+        return;
+    }
+    _file.open(_filename);
+    if (!_file.is_open()) {
+        std::cerr << program_invocation_name << ": " << _filename << ": " << std::strerror(errno) << std::endl;
+        return;
+    }
+    _isOpen = true;
+}
+
+bool nts::Config::isOpen() const {
+    return _isOpen;
+}
+
+std::ifstream &nts::Config::getFile() {
+    return _file;
+}
+
+const std::string &nts::Config::getFilename() const {
+    return _filename;
+}
