ef8bcdca57e03b8b4058ec7ae8df85615ee3ed82
Clement-Z4RM <clement.lienard@epitech.eu>
2024-02-16 19:09:27 +0100

    feat: add `Circuit` class that parse configuration file from `Config` class

---
+++ src/Circuit/Circuit.cpp
@@ -0,0 +1,95 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** Circuit.cpp
+*/
+
+#include <utility>
+#include <regex>
+#include "../Utilities/Utilities.hpp"
+#include "../Components/Factory/ComponentsFactory.hpp"
+#include "Circuit.hpp"
+
+const std::string nts::Circuit::Error::ERRORS[] = {
+        "The specified configuration is invalid (no file open)",
+        "Unknown part name",
+        "Unknown component type",
+        "Invalid chipset line format",
+        "Invalid link line format"
+};
+
+nts::Circuit::Error::Error(std::string message) : _message(std::move(message)) {}
+
+const char *nts::Circuit::Error::what() const noexcept {
+    return _message.c_str();
+}
+
+nts::Circuit::Circuit() : _isLoaded(false) {
+    _partsFunctions["chipsets"] = &nts::Circuit::_chipsetFunction;
+    _partsFunctions["links"] = &nts::Circuit::_linkFunction;
+}
+
+nts::Circuit::Circuit(nts::Config &config) : Circuit() {
+    loadConfig(config);
+}
+
+bool nts::Circuit::isLoaded() const {
+    return _isLoaded;
+}
+
+#include <iostream>
+void nts::Circuit::loadConfig(nts::Config &config) {
+    if (!config.isOpen())
+        throw Error(Error::ERRORS[Error::ClosedFileConfig]);
+
+    std::ifstream &file = config.getFile();
+    std::string currentPart;
+    std::vector<std::string> tokens;
+    std::string buffer;
+    std::string::size_type hashtagIndex;
+
+    while (std::getline(file, buffer)) {
+        hashtagIndex = buffer.find('#');
+        if (hashtagIndex != std::string::npos) {
+            buffer[hashtagIndex] = '\0';
+            buffer.resize(hashtagIndex);
+        }
+        if (buffer.empty() || std::regex_match(buffer, std::regex("^[[:blank:]]+$")))
+            continue;
+        if (buffer.starts_with('.') && buffer.ends_with(':')) {
+            currentPart = buffer.substr(1, buffer.size() - 2);
+            if (_partsFunctions.find(currentPart) == _partsFunctions.end())
+                throw Error(Error::ERRORS[Error::UnknownPart] + " '" + currentPart + "'");
+            continue;
+        }
+        (this->*(_partsFunctions[currentPart]))(buffer);
+    }
+    _isLoaded = true;
+    for (const auto &item: _chipsets) {
+        std::cout << item.first << ": ";
+        item.second->simulate(1);
+    }
+}
+
+void nts::Circuit::_chipsetFunction(const std::string &line) {
+    if (!std::regex_match(line, std::regex(R"(^[[:blank:]]*[[:alnum:]]{2,}[[:blank:]]+[[:alnum:]]+[[:blank:]]*$)")))
+        throw Error(Error::ERRORS[Error::InvalidChipsetLineFormat] + " '" + line + "'");
+
+    std::vector<std::string> tokens = Utilities::splitLine(line);
+    if (!ComponentsFactory::isComponentExists(tokens[COMPONENT]))
+        throw Error(Error::ERRORS[Error::UnknownComponentType] + " '" + tokens[COMPONENT] + "'");
+    if (_chipsets.contains(tokens[NAME]))
+        throw Error("Component '" + tokens[NAME] + "' already exists");
+    _chipsets[tokens[NAME]] = ComponentsFactory::createComponent(tokens[COMPONENT]);
+}
+
+void nts::Circuit::_linkFunction([[maybe_unused]] const std::string &line) {
+    if (!std::regex_match(line, std::regex(R"(^[[:blank:]]*[[:alnum:]]+:\d+[[:blank:]]+[[:alnum:]]+:\d+[[:blank:]]*$)")))
+        throw Error(Error::ERRORS[Error::InvalidLinkLineFormat] + " '" + line + "'");
+
+    std::vector<std::string> tokens = Utilities::splitLine(line);
+    std::vector<std::string> pin1 = Utilities::splitLine(tokens[PIN_1], ':');
+    std::vector<std::string> pin2 = Utilities::splitLine(tokens[PIN_2], ':');
+    // TODO: Implement link
+}
---
+++ src/Circuit/Circuit.hpp
@@ -0,0 +1,70 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** Circuit.hpp
+*/
+
+#ifndef NANOTEKSPICE_CIRCUIT_HPP_
+#define NANOTEKSPICE_CIRCUIT_HPP_
+
+#include <exception>
+#include <functional>
+#include <memory>
+#include <unordered_map>
+#include "../Parsing/Config/Config.hpp"
+#include "IComponent.hpp"
+
+#define COMPONENT 0
+#define NAME      1
+#define PIN_1     0
+#define PIN_2     1
+#define PIN       1
+
+namespace nts {
+    // TODO: Implement IComponent and uncomment the inheritance (or not, IDK)
+    // TODO: throw errors if function is called before loadConfig
+    class Circuit /*: public IComponent*/ {
+    public:
+        class Error : public std::exception {
+        public:
+            enum ErrorType {
+                ClosedFileConfig,
+                UnknownPart,
+                UnknownComponentType,
+                InvalidChipsetLineFormat,
+                InvalidLinkLineFormat
+            };
+
+            static const std::string ERRORS[];
+
+            explicit Error(std::string message);
+
+            [[nodiscard]] const char *what() const noexcept override;
+
+        private:
+            const std::string _message;
+        };
+
+    public:
+        Circuit();
+
+        explicit Circuit(Config &config);
+
+        [[nodiscard]] bool isLoaded() const;
+
+        void loadConfig(Config &config);
+
+    private:
+        bool _isLoaded;
+        std::unordered_map<std::string, void (Circuit::*)(const std::string &)> _partsFunctions;
+
+        std::unordered_map<std::string, std::unique_ptr<IComponent>> _chipsets;
+
+        void _chipsetFunction(const std::string &line);
+
+        void _linkFunction(const std::string &line);
+    };
+}
+
+#endif /* !NANOTEKSPICE_CIRCUIT_HPP_ */
