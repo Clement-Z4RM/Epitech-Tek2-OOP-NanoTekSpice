0d4ef5fb5578609b865d5a7e438b097b78fcfda8
josephinecr <josephine.chicouras@epitech.eu>
2024-02-19 18:36:45 +0100

    feat: add component 4030

---
+++ src/Components/Gates/4030/Component4030.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** Component4030.hpp
+*/
+
+#ifndef COMPONENT4030_HPP
+#define COMPONENT4030_HPP
+
+#include "../../AComponent.hpp"
+
+namespace nts {
+    class Component4030 : public AComponent {
+    public:
+        Tristate compute(std::size_t pin) override;
+    };
+}
+
+#endif //COMPONENT4030_HPP
